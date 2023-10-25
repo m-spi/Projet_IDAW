d2773bc1f7e943d06d4a87a5deee8ebc5714b20e
m-spi <matis.spinelli30@gmail.com>
2023-10-25 09:44:57 +0200

    SQL corrigé

--- TraqueTaBouffe.sql
+++ TraqueTaBouffe.sql
@@ -4,14 +4,12 @@
 /*==============================================================*/
 
 
-
-
 /*==============================================================*/
 /* Table : ALIMENTS                                             */
 /*==============================================================*/
 create table ALIMENTS
 (
-   ID_ALIMENT           int not null  comment '',
+   ID_ALIMENT           int not null AUTO_INCREMENT comment '',
    INDICE_NOVA          int  comment '',
    NOM_ALIMENT          varchar(1024) not null  comment '',
    ISLIQUID             bool not null  comment '',
@@ -23,9 +21,9 @@ create table ALIMENTS
 /*==============================================================*/
 create table ALIMENT_CONSOMME
 (
-   ID_REPAS             int not null  comment '',
-   ID_ALIMENT           int not null  comment '',
-   ID_USER              int not null  comment '',
+   ID_REPAS             int not null AUTO_INCREMENT comment '',
+   ID_ALIMENT_FK           int not null  comment '',
+   ID_USER_FK              int not null  comment '',
    DATE                 datetime not null  comment '',
    QUANTITE             float not null  comment '',
    primary key (ID_REPAS)
@@ -36,10 +34,10 @@ create table ALIMENT_CONSOMME
 /*==============================================================*/
 create table COMPOSITION
 (
-   ID_COMPOSANT         int not null  comment '',
-   ID_ALIMENT           int not null  comment '',
+   ID_COMPOSANT_FK         int not null  comment '',
+   ID_ALIMENT_FK           int not null  comment '',
    QUANTITE_DU_COMPOSANT float not null  comment '',
-   primary key (ID_COMPOSANT, ID_ALIMENT)
+   primary key (ID_COMPOSANT_FK, ID_ALIMENT_FK)
 );
 
 /*==============================================================*/
@@ -47,10 +45,10 @@ create table COMPOSITION
 /*==============================================================*/
 create table EST_COMPOSE
 (
-   ID_NUTRIMENT         int not null  comment '',
-   ID_ALIMENT           int not null  comment '',
+   ID_NUTRIMENT_FK         int not null  comment '',
+   ID_ALIMENT_FK           int not null  comment '',
    POURCENTAGE          float not null  comment '',
-   primary key (ID_NUTRIMENT, ID_ALIMENT)
+   primary key (ID_NUTRIMENT_FK, ID_ALIMENT_FK)
 );
 
 /*==============================================================*/
@@ -58,7 +56,7 @@ create table EST_COMPOSE
 /*==============================================================*/
 create table NUTRIMENTS
 (
-   ID_NUTRIMENT         int not null  comment '',
+   ID_NUTRIMENT         int not null AUTO_INCREMENT comment '',
    NOM_NUTRIMENT        varchar(1024) not null  comment '',
    primary key (ID_NUTRIMENT)
 );
@@ -68,7 +66,7 @@ create table NUTRIMENTS
 /*==============================================================*/
 create table USER
 (
-   ID_USER              int not null  comment '',
+   ID_USER              int not null AUTO_INCREMENT comment '',
    NOM                  varchar(1024) not null  comment '',
    AGE                  int not null  comment '',
    ISMALE               bool not null  comment '',
@@ -79,21 +77,21 @@ create table USER
    primary key (ID_USER)
 );
 
-alter table ALIMENT_CONSOMME add constraint FK_ALIMENT__CONSOMME_USER foreign key (ID_USER)
+alter table ALIMENT_CONSOMME add constraint FK_ALIMENT__CONSOMME_USER foreign key (ID_USER_FK)
       references USER (ID_USER) on delete restrict on update restrict;
 
-alter table ALIMENT_CONSOMME add constraint FK_ALIMENT__EST_ALIMENTS foreign key (ID_ALIMENT)
+alter table ALIMENT_CONSOMME add constraint FK_ALIMENT__EST_ALIMENTS foreign key (ID_ALIMENT_FK)
       references ALIMENTS (ID_ALIMENT) on delete restrict on update restrict;
 
-alter table COMPOSITION add constraint FK_COMPOSIT_COMPOSITI_ALIMENTS foreign key (ID_COMPOSANT)
+alter table COMPOSITION add constraint FK_COMPOSIT_COMPOSITI_ALIMENTS foreign key (ID_COMPOSANT_FK)
       references ALIMENTS (ID_ALIMENT) on delete restrict on update restrict;
 
-alter table COMPOSITION add constraint FK_COMPOSIT_COMPOSITI_ALIMENTS foreign key (ID_ALIMENT)
+alter table COMPOSITION add constraint FK2_COMPOSIT_COMPOSITI_ALIMENTS foreign key (ID_ALIMENT_FK)
       references ALIMENTS (ID_ALIMENT) on delete restrict on update restrict;
 
-alter table EST_COMPOSE add constraint FK_EST_COMP_EST_COMPO_NUTRIMEN foreign key (ID_NUTRIMENT)
+alter table EST_COMPOSE add constraint FK_EST_COMP_EST_COMPO_NUTRIMEN foreign key (ID_NUTRIMENT_FK)
       references NUTRIMENTS (ID_NUTRIMENT) on delete restrict on update restrict;
 
-alter table EST_COMPOSE add constraint FK_EST_COMP_EST_COMPO_ALIMENTS foreign key (ID_ALIMENT)
+alter table EST_COMPOSE add constraint FK2_EST_COMP_EST_COMPO_ALIMENTS foreign key (ID_ALIMENT_FK)
       references ALIMENTS (ID_ALIMENT) on delete restrict on update restrict;
 
