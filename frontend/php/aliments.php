95ca5643c81a64aead55d373ee921473678499b8
donia <donia.sakji@gmail.com>
2023-11-07 08:58:08 +0100

    ajout du get delete et add sur aliment

--- frontend/php/aliments.php
+++ frontend/php/aliments.php
@@ -3,41 +3,27 @@
         <div class="container-fluid px-4">
             <div class="rr margin-bottom">
                 <h1 class="mt-4">Mes aliments</h1>
-                <button class="side-btn" id="selectedBtn">Ajouter <i class="fas fa-plus-circle"></i></button>
+                <button class="side-btn" id="ajouterAlimentsBtn">Ajouter <i class="fas fa-plus-circle"></i></button>
             </div>
-            <?php
-            require_once ('codeFactorisé/dataTables.php');
-            $dataJSON =
-                '[
-        {
-            "id": 1,
-            "nom_aliment": "Pommes",
-            "Date de consommation": "2023-10-30",
-            "indice_nova": 2,
-            "energie_kcal": 52.0,
-            "sucre": 10.4,
-            "proteine": 0.26,
-            "fibre_alimentaire": 2.4,
-            "matiere_grasse": 0.2,
-            "alcool": 0.0
-        },
-        {
-            "id": 2,
-            "nom_aliment": "Bananes",
-            "Date de consommation": "2023-10-29",
-            "indice_nova": 3,
-            "energie_kcal": 89.0,
-            "sucre": 12.2,
-            "proteine": 1.1,
-            "fibre_alimentaire": 2.6,
-            "matiere_grasse": 0.3,
-            "alcool": 0.0
-        }
-    ]';
-
-            displayDataTable($dataJSON, 'Mes aliments');
-
-            ?>
+            <div class="card mb-4 ">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i> Mes aliments
+                </div>
+                <div class="card-body">
+                    <table id="alimentsDataTable" class="hover cell-border order-column row-border ">
+                        <thead>
+                        <tr>
+                            <th>Aliment</th>
+                            <th> Nutriments (pour 100g ou 100ml)</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
         </div>
     </main>
+    <script type="text/javascript" src="../js/aliments.js">
+    </script>
 
--- frontend/php/codeFactorisé/header.php
+++ frontend/php/codeFactorisé/header.php
@@ -6,11 +6,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title> Traque ta bouffe </title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="../../node_modules/startbootstrap-sb-admin/dist/css/styles.css" rel="stylesheet" />
         <link href="../css/main.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+        <title> Traque ta bouffe </title>
     </head>
     <body class="sb-nav-fixed">
     <header> <!--header de la page (sans le menu)-->
