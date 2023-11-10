d9385d1ea47663f0fd712234466dacb79e4a37f4
donia <donia.sakji@gmail.com>
2023-11-10 14:12:06 +0100

    dashboard fini ajout des graphiques

--- frontend/js/dashBoard.js
+++ frontend/js/dashBoard.js
@@ -1,23 +1,51 @@
 $(document).ready(function () {
+    console.log("22");
+    var click=0;
     var table = $('#dashBoardDataTable').DataTable();
-    filterDataByDuration(7, table)
-        .then(function (bilan) {
-            console.log("Bilan: ", bilan);
+    $('.chart-button').on('click', function () {
+        if (click ==0) {
+            alert("Veuillez d'abord selectionner la durée du bilan dans \"Selectioner un bilan\"");
+        }
+
+    });
+
+    filterDataByDuration(7, table,"sucre")// par default on affiche l'évolution du sucre
+        .then(function ({total, dataByNutrient }) {
+            console.log("Bilan: ", total);
             var userGender = returnUserGender(id_user);
             console.log("usergender :", userGender);
-            Recommendations(7, bilan, userGender);
+            Recommendations(7, total, userGender);
+            //graphique
+            console.log("data",dataByNutrient);
+            createChart(7,dataByNutrient,"sucre");
+            createBarChart(7,dataByNutrient,"sucre");
 
             $('.duration-button').on('click', function () {
+                click++;
                 var duration = $(this).data('duration');
-                filterDataByDuration(duration, table)
-                    .then(function (bilan) {
-                        console.log("Bilan: ", bilan);
+                $('#selectedBtn').text('Bilan pour les ' + duration + ' derniers jours');
+                filterDataByDuration(duration,table,"sucre")
+                    .then(function ({total, dataByNutrient }) {
                         var userGender = returnUserGender(id_user);
-                        Recommendations(duration, bilan, userGender);
+                        Recommendations(duration, total, userGender);
                     })
                     .catch(function (error) {
                         console.error("Erreur lors de la récupération des données : ", error);
                     });
+                $('.chart-button').on('click', function () {
+                    console.log("click");
+                    var nutriment = $(this).data('nutriment');
+                    $('#selectedChartBtn').text('Bilan consommation '+nutriment);
+                    filterDataByDuration(duration,table,nutriment)
+                        .then(function ({total, dataByNutrient }) {
+                            createChart(duration, dataByNutrient,nutriment);
+                            createBarChart(duration, dataByNutrient,nutriment);
+                        })
+                        .catch(function (error) {
+                            console.error("Erreur lors e la récupération des données pour le graphique : ", error);
+                        });
+
+                });
             });
         })
         .catch(function (error) {
@@ -25,7 +53,7 @@ $(document).ready(function () {
         });
 });
 
-function filterDataByDuration(duration, table) {
+function filterDataByDuration(duration, table, nutriment) {
     return new Promise(function (resolve, reject) {
         $.ajax({
             url: prefixeEndpoint + "/backend/journal.php/user/" + id_user,
@@ -62,17 +90,22 @@ function filterDataByDuration(duration, table) {
                                 return alim.id == plat.id_aliment;
                             });
                             //console.log("aliment"+ JSON.stringify(platTrouve));
-                            total.sucre += parseFloat(platTrouve.sucre);
-                            total.sel += parseFloat(platTrouve.sel);
-                            total.energie += parseFloat(platTrouve.energie_kcal);
-                            total.fibre += parseFloat(platTrouve.fibre_alimentaire);
-                            total.matiereGrasse += parseFloat(platTrouve.matieres_grasses);
+                            total.sucre +=( ((platTrouve.sucre)*plat.quantite)/100);
+                            total.sel +=  parseFloat((platTrouve.sel*plat.quantite)/100);
+                            total.energie +=  parseFloat((platTrouve.energie_kcal*plat.quantite)/100);
+                            total.fibre +=  parseFloat((platTrouve.fibre_alimentaire*plat.quantite)/100);
+                            total.matiereGrasse += parseFloat((platTrouve.matieres_grasses*plat.quantite)/100);
+                            total.proteines += parseFloat((platTrouve.proteines*plat.quantite)/100);
                         });
                         Object.keys(total).forEach(function (key) {
                             total[key] = parseFloat(total[key]).toFixed(2);
                         });
+                        console.log(journalFiltre,aliment);
+                        var dataByNutrient = prepareDataForChart(journalFiltre,aliment,nutriment);
+                        console.log("dataNutriment   ",dataByNutrient);
                         updateDataTable(total, table);
-                        resolve(total);
+                        resolve({ total: total, dataByNutrient: dataByNutrient });
+
                     })
                     .fail(function (error) {
                         reject(error);
@@ -191,3 +224,139 @@ function Recommendations(duration, bilanData, userGender) {
     }
     $('#recommendationContainer').html(recommendationHtml);
 }
+
+//fonction pour Dashboard graphique
+
+function generateLabels(duration) { //tableau avec les dates
+    var labels = [];
+    var today = new Date();
+
+    for (var i = duration - 1; i >= 0; i--) {
+        var date = new Date(today);
+        date.setDate(today.getDate() - i);
+        labels.push((date.toISOString().split('T')[0]));
+    }
+
+    return labels;
+}
+function changeDateType(dates) {
+    var months = [
+        "janv", "fév", "mars", "avril", "mai", "juin",
+        "juil", "août", "sept", "oct", "nov", "déc"
+    ];
+
+    return dates.map(function (dateStr) {
+        var parts = dateStr.split("-");
+        var day = parseInt(parts[2], 10);
+        var month = months[parseInt(parts[1], 10) - 1];
+        return day + " " + month;
+    });
+}
+function prepareDataForChart(journal, aliment, selectedNutriment) {
+    var dataNutriment = {};
+    var result = [];
+
+    journal.forEach(function (entry) {
+        var platTrouve = aliment.find(function (alim) {
+            return alim.id == entry.id_aliment;
+        });
+
+        if (platTrouve && platTrouve[selectedNutriment] !== undefined) {
+            var dateKey = entry.date.split(' ')[0];
+
+            if (!dataNutriment[dateKey]) {
+                dataNutriment[dateKey] = 0;
+            }
+
+            // Ajoute la quantité spécifique du nutriment sélectionné
+            dataNutriment[dateKey] += parseFloat(platTrouve[selectedNutriment]) * entry.quantite;
+        }
+    });
+
+    // Convertit l'objet en tableau avec des entrées non redondantes
+    for (var date in dataNutriment) {
+        result.push({
+            date: date,
+            quantity: dataNutriment[date].toFixed(2),
+        });
+    }
+
+    return result;
+}
+
+function getNutrient(nutrientData, labels) {
+    console.log("nutriment",nutrientData);
+    var data = new Array(labels.length).fill(0);
+
+    nutrientData.forEach(function (entry) {
+        var index = labels.indexOf(entry.date);
+
+        if (index !== -1) {
+            data[index] = entry.quantity;
+        }
+    });
+    console.log("getNutrient",data);
+    return data;
+}
+var ctx = document.getElementById('myBilanChart').getContext('2d');
+var barCtx = document.getElementById('myBarBilanChart').getContext('2d');
+
+function createChart(duration, data, nutriment) {
+
+    var labels = generateLabels(duration);
+    console.log("createChart", getNutrient(data,labels));
+
+
+    var dataset = {
+        label: nutriment,
+        data: getNutrient(data,labels),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+    };
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: changeDateType(labels),
+            datasets: [dataset]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+function createBarChart(duration, data, nutriment) {
+
+    var labels = generateLabels(duration);
+    console.log("createChart", getNutrient(data,labels));
+
+
+    var dataset = {
+        label: nutriment,
+        data: getNutrient(data,labels),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+    };
+
+    var myChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: changeDateType(labels),
+            datasets: [dataset]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
--- frontend/php/dashBoard.php
+++ frontend/php/dashBoard.php
@@ -1,3 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid px-4">
@@ -17,43 +18,10 @@
             <div class="row" id="recommendationContainer"> <!--Etiquettes-->
 
             </div>
-            <!---<div class="rr">
-                <h1 class="mt-4 h3Size">Mon bilan en graphiques</h1>
-                <div class="dropdown">
-                    <button class="side-btn smallSize"  id="selectedBtn">Sélectionner une donnée<span class="arrow-down"></span></button>
-                    <div class="dropdown-content ">
-                        <a href="bilan.php?jours=7">sucre</a>
-                        <a href="bilan.php?jours=7">sel</a>
-                        <a href="bilan.php?jours=14">matière grasse</a>
-                        <a href="bilan.php?jours=30">alcool</a>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-6">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <i class="fas fa-chart-area me-1"></i>
-                            Area Chart Example
-                        </div>
-                        <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                    </div>
-                </div>
-                <div class="col-xl-6">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <i class="fas fa-chart-bar me-1"></i>
-                            Bar Chart Example
-                        </div>
-                        <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                    </div>
-                </div>
-            </div>-->
-            <div class="container-fluid px-4">
-                <div class="rr margin-bottom">
-                    <h1 class="mt-4">Mon Bilan</h1>
 
-                </div>
+
+            <div class="container-fluid px-4">
+
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table me-1"></i> Bilan nutritionnel
@@ -65,7 +33,7 @@
                             <th>Energie (kcal)</th>
                             <th>Sucre (g)</th>
                             <th>Sel (g)</th>
-                            <th>Matières grasse (g)</th>
+                            <th>Matières grasses (g)</th>
                             <th>Fibre alimentaire</th>
                             <th>Protéines</th>
                             <!--<th>Alcool</th>-->
@@ -77,6 +45,40 @@
                     </table>
                 </div>
         </div>
+                <div class="rr">
+                    <h1 class="mt-4 graphiqueTitle">Mon bilan en graphiques</h1>
+                    <div class="dropdown">
+                        <button class="side-btn smallSize"  id="selectedChartBtn">Sélectionner une donnée<span class="arrow-down"></span></button>
+                        <div class="dropdown-content ">
+                            <button class="chart-button" data-nutriment="energie_kcal">Energie (kcal)</button>
+                            <button class="chart-button" data-nutriment="sucre">Sucre (g)</button>
+                            <button class="chart-button" data-nutriment="sel">Sel (g)</button>
+                            <button class="chart-button" data-nutriment="matieres_grasses">Matières grasses (g)</button>
+                            <button class="chart-button" data-nutriment="fibre_alimentaire">Fibre alimentaire (g)</button>
+                            <button class="chart-button" data-nutriment="proteines">Protéines (g)</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-chart-area me-1"></i>
+                                Bilan graphique
+                            </div>
+                            <div class="card-body"><canvas id="myBilanChart" width="100%" height="40"></canvas></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-chart-bar me-1"></i>
+                                Bilan graphique en barre
+                            </div>
+                            <div class="card-body"><canvas id="myBarBilanChart" width="100%" height="40"></canvas></div>
+                        </div>
+                    </div>
+                </div>
     </main>
     <?php
     //session_start(); // Démarrez la session si ce n'est pas déjà fait
@@ -91,8 +93,7 @@
         console.log("id_user "+id_user);
     </script>
     <script src="../js/config.js"></script>
-    <script src="../js/dashboard/dashBoard.js"></script>
-    <!--<script src="../js/dashboard/afficherRecommandation.js"></script>-->
+    <script src="../js/dashBoard.js"></script>
 
 
 
